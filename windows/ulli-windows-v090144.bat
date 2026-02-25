65166a320ecec76236fb64c00254cb75819264c2
rltvty2 <32552121+rltvty2@users.noreply.github.com>
2026-02-25 08:42:32 +0100

    Update and rename ulli-windows-v090144.bat to ulli-windows-v090145.bat

--- windows/ulli-windows-v090144.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0ulli-windows-v090144.ps1"
-pause
-
-
---
+++ windows/ulli-windows-v090145.bat
@@ -0,0 +1,6 @@
+@echo off
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0ulli-windows-v090145.ps1"
+pause
+
+
+
